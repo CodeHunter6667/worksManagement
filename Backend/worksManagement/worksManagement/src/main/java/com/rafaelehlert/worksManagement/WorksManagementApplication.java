03493fae47078b8cd8d1e79695a8493c55f26ed9
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-09-18 19:42:37 -0300

    initial commit

---
+++ Backend/worksManagement/worksManagement/src/main/java/com/rafaelehlert/worksManagement/WorksManagementApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelehlert.worksManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorksManagementApplication.class, args);
+	}
+
+}
---
+++ Backend/worksManagement/worksManagement/src/test/java/com/rafaelehlert/worksManagement/WorksManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelehlert.worksManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorksManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
